00c1ff355f7beea74b136bd2ba21ed0c94d9db65
Kevin Feng <kfengc27@gmail.com>
2025-02-28 02:48:50 +1100

    add files

--- app/api/common.ts
+++ app/api/common.ts
@@ -7,6 +7,9 @@ const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
 
 export async function requestOpenai(req: NextRequest) {
   const apiKey = req.headers.get("token");
+  // console.log(req.headers.get("token"));
+  console.log("测试");
+  // const apiKey = "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";
   const openaiPath = req.headers.get("path");
 
   let baseUrl = BASE_URL;
--- app/constant.ts
+++ app/constant.ts
@@ -6,3 +6,4 @@ export const UPDATE_URL = `${REPO_URL}#keep-updated`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
+export const SUMMARIZE_MODEL = "gpt-4o";
