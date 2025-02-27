b7ce421ec0edfc632d389d79c5c0f27f5f29bfa6
Kevin Feng <kfengc27@gmail.com>
2025-02-28 09:58:07 +1100

    add files

--- app/api/common.ts
+++ app/api/common.ts
@@ -1,16 +1,18 @@
 import { NextRequest } from "next/server";
 
-const OPENAI_URL = "api.openai-sb.com";
+const OPENAI_URL = "api.openai.com";
 const DEFAULT_PROTOCOL = "https";
 const PROTOCOL = process.env.PROTOCOL ?? DEFAULT_PROTOCOL;
 const BASE_URL = process.env.BASE_URL ?? OPENAI_URL;
 
 export async function requestOpenai(req: NextRequest) {
   const apiKey = req.headers.get("token");
-  // console.log(req.headers.get("token"));
-  console.log("测试");
+
   // const apiKey = "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";
   const openaiPath = req.headers.get("path");
+
+  console.log("什么");
+  console.log(openaiPath);
 
   let baseUrl = BASE_URL;
 
